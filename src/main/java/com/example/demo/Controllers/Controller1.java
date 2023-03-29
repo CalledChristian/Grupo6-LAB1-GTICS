35313c06b5af99dd9c805387f4d4a619554c2d8f
sebas1310 <a20203368@pucp.edu.pe>
2023-03-28 21:44:44 -0500

    homepage

--- src/main/java/com/example/demo/Controllers/Controller1.java
+++ src/main/java/com/example/demo/Controllers/Controller1.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller1 {
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/homepage")
     public String homepage(){
 
-        return "registroTelec";
+        return "homepage";
     }
 }
