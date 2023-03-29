152d4f435b3175b285e3d405da111a4ec043a89f
unknown <calledchristian79@gmail.com>
2023-03-29 02:22:55 -0500

    commit 3

--- src/main/java/com/example/demo/Controllers/Controller1.java
+++ src/main/java/com/example/demo/Controllers/Controller1.java
@@ -1,4 +1,27 @@
 package com.example.demo.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping(value="/",method = RequestMethod.GET)
 public class Controller1 {
+
+
+    @GetMapping(value="/login")
+
+    public String logeo(){
+
+        return "";
+    }
+
+
+
+    @GetMapping(value="/newUser")
+    public String nuevoUsuario(){
+
+        return "";
+    }
 }
