726929af87241787a9eb2e836e5fa939e82f28a5
unknown <calledchristian79@gmail.com>
2023-03-29 02:48:45 -0500

    commit gg

--- src/main/java/com/example/demo/Controllers/Controller1.java
+++ src/main/java/com/example/demo/Controllers/Controller1.java
@@ -1,12 +1,41 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.entities.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controller1 {
     @GetMapping("/login")
     public String inicio(){
         return "registroTelec";
+    }
+
+
+
+
+
+    @GetMapping("/newUser")
+    public String nuevoUsuario(){
+
+        return "formulario";
+    }
+
+
+    @PostMapping("/guardar")
+    public String guardarUsuario(@RequestParam("nombreCompleto") String nombreCompleto,
+                                @RequestParam("usuario") String user,
+                                 @RequestParam("correo") String correo,
+                                 @RequestParam("contrasena") String contrasena,
+                                @RequestParam("contrasena_r") String contrasena_r){
+
+        Usuario usuario = new Usuario();
+       usuario.setNombreCompleto(nombreCompleto);
+       usuario.setUsuario(user);
+       usuario.setCorreo(correo);
+       usuario.setContrasena(contrasena);
+        return "datosUsuario";
     }
 }
---
+++ src/main/java/com/example/demo/entities/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.demo.entities;
+
+public class Usuario {
+    private String nombreCompleto;
+    private String usuario;
+    private String correo;
+    private String contrasena;
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
