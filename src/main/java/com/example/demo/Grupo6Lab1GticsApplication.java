b32d2e341c569dca330bdf2eacbbc5737025f95e
unknown <calledchristian79@gmail.com>
2023-03-29 02:06:34 -0500

    first commit

---
+++ src/main/java/com/example/demo/Grupo6Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo6Lab1GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo6Lab1GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo/Grupo6Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo6Lab1GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
